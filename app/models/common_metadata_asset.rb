a8434ec61472f5ebb1968bf24aad8a3e5dfb8225
Naomi Dushay <ndushay@stanford.edu>
2012-05-15 15:22:44 -0700

    removed deprecation of app/model/common_metadata_asset because it is used in testing.  it will be renamed

--- app/models/common_metadata_asset.rb
+++ app/models/common_metadata_asset.rb
@@ -1,11 +1,6 @@
-# @deprecated This is intended as an EXAMPLE of mixing in model behavior and will be moved to hydra-wiki documentation.  This will be removed no later than 6.x
-# EXAMPLE of a basic model that conforms to Hydra commonMetadata cModel and has basic MODS metadata (currently "Article" is the MODS exemplar)
+# Used to test default view partials;  this is a basic hydra model with no specific views
+# EXAMPLE of a basic model that conforms to Hydra commonMetadata cModel and has basic MODS metadata
 class CommonMetadataAsset < ActiveFedora::Base
-  
-  def initialize
-    ActiveSupport::Deprecation.warn("CommonMetadataAsset is deprecated and will be removed in release 5 or 6;  this exemplar code has been moved into wiki documentation here:  https://github.com/projecthydra/hydra-head/wiki/Models---Some-Examples")
-    super
-  end
   
   # declares a rightsMetadata datastream with type Hydra::Datastream::RightsMetadata
   #  basically, it is another expression of
